a310328c7de2bc458612d073d2b340341464c7e3
Udit Desai <desaiuditd@gmail.com>
2026-04-12 20:08:05 +1000

    Fix brew path after install

--- run_once_before_install-managers.sh
+++ run_once_before_install-managers.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 
 # Install Homebrew (Temporary. Until justrach/nanobrew#110 is solved.)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+
+# Uninstall Homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/uninstall.sh)"
+# sudo rm -rf /etc/paths.d/homebrew
+# sudo rm -rf /opt/homebrew
 
 # Install Nanobrew
 # curl -fsSL https://nanobrew.trilok.ai/install | bash
